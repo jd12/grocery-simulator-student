3259c3ce5342977c97d791e9419bbb6f28c8ca37
J.D. DeVaughn-Brown <jd@chadwickschool.org>
2022-01-25 08:25:49 -0800

    Update LinkedQueue.java

--- src/main/java/structures/LinkedQueue.java
+++ src/main/java/structures/LinkedQueue.java
@@ -1,7 +1,7 @@
 package structures;
 
 public class LinkedQueue<T> implements QueueInterface<T> {
-    
+
     private LinearNode<T> front;
 
     @Override
@@ -33,19 +33,19 @@ public class LinkedQueue<T> implements QueueInterface<T> {
         // TODO Auto-generated method stub
         return 0;
     }
-    
+
     @Override
     public String toString() {
-        // use stringbuilder because it's faster and more memory efficient
+        // use StringBuilder because it's faster and more memory efficient
         StringBuilder result = new StringBuilder();
         result.append("[");
         // start at the front of the list
-        LinearNode<T> copy = this.front; // this line will work once you copy in your LinearNode class
+        LinearNode<T> copy = this.front;
+        // TODO: Write loop that goes through queue and executes the following two lines
         result.append(copy.getElement().toString());
         // move to the next node
         copy = copy.getNext();
-        
-        // 
+
         return result.append("]").toString();
     }
 
